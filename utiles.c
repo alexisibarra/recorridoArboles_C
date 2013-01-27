45dc8351587d091cc08b066779ac02be14bbcf55
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2013-01-26 22:09:48 -0430

    Implementación y libreria de algunas funciones útiles para la tarea. En este commit se agregan funciones para obtener la cabeza y la cola de un string

---
+++ utiles.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  utiles.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26/01/13 21:59:33
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexis Ibarra (ai), ar.ibarrasalas@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "utiles.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *
+cabeza (char * string)
+{
+	return strndup(string+0, 1);
+}
+
+char *
+cola (char * string)
+{
+	return strndup(string+1,strlen(string));
+}
---
+++ utiles.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  utiles.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26/01/13 21:58:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexis Ibarra (ai), ar.ibarrasalas@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototipos de funciones definidas en listas.c */
+//extern Arbol Insertar(Arbol a, NODO *, char[]);
+extern char *cabeza(char*);
+extern char *cola(char*);
+
