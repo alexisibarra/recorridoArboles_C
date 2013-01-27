5e2f9c3a2d4850a376c033d6a76fc45a15fdadc0
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2013-01-27 09:57:37 -0430

    Agregadas cabeceras de documentación de algunas funciones, impresión con recorrido preorden para ir probando y la inserción está lista

--- arbol.c
+++ arbol.c
@@ -72,22 +72,44 @@ nuevoNodo(int elem)
  */
 void
 Imprimir (Arbol a){
-	printf("Elem: %d\n", a->Elem);
+//	printf("Elem: %d\n", a->Elem);
+  if (a != NULL) {
+		printf("%d\n",a->Elem);
+    Imprimir(a->izq);
+    Imprimir(a->der);
+  }
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  ramaIzq
+ *  Description:  Devuelve la rama izquierda del arbol
+ * =====================================================================================
+ */
 Arbol
 ramaIzq(Arbol a)
 {
 	return a->izq;
 }
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  ramaDer
+ *  Description:  Devuelve la rama derecha del arbol
+ * =====================================================================================
+ */
 Arbol
 ramaDer(Arbol a)
 {
 	return a->der;
 }
 
-
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Insertar
+ *  Description:  Inserta un entero positivo en el arbol
+ * =====================================================================================
+ */
 void
 Insertar(Arbol a, int elem, char *camino)
 {
@@ -95,150 +117,17 @@ Insertar(Arbol a, int elem, char *camino)
 	char *resto = (char*)cola(camino);
 	if (strcmp(siguiente,"I")==0){
 		printf("Izquierda\n");
-		Insertar(a,elem,resto);
+		a->izq=nuevoArbol();
+		Insertar(a->izq,elem,resto);
 	}
 	if (strcmp(siguiente,"D")==0 ) {
 		printf("Derecha\n");
-		Insertar(a,elem,resto);
+		a->der=nuevoArbol();
+		Insertar(a->der,elem,resto);
 	}
 	if (strcmp(siguiente,"")==0 ) {
+		a->Elem=elem;
 		printf("Insertar elem\n");
 	}
 }
 
-///* 
-// * ===  FUNCTION  ======================================================================
-// *       Nombre:  EliminarEst
-// *  Descripción:  Elimina un estudiante de la lista identificado por su carnet.
-// *  							Si el estudiante no se encuentra en la lista ésta no se modifica y se 
-// *  							indica al usuario.
-// *			Entrada:	Lista l en la cual se eliminará el estudiante.
-// *								Entero Carnet del estudiante a eliminar
-// *			 Salida:  Lista con la eliminación realizada
-// * =====================================================================================
-// */
-//Lista
-//EliminarEst (Lista l, int Carnet)
-//{
-//  Lista Temp, anterior;
-//
-//  if (l == NULL)
-//    return NULL;		/* lista vacia */
-//  if (Carnet == l->Carnet)
-//    {				/* Caso especial: el empleado a
-//				   eliminar esta de primero */
-//      Temp = l;
-//      l = l->proximo;		/* la lista empieza ahora en el segundo */
-//      free (Temp->Nombre);	/* libera espacio del Nombre primero! */
-//      free (Temp);		/* libera espacio de Empleado eliminado */
-//      return l;
-//    }
-//  else
-//    {				/* empleado no esta de primero */
-//      anterior = l;
-//      Temp = l->proximo;
-//      /* Ahora recorre la lista buscando por el campo  Cedula */
-//      while (Temp != NULL && Temp->Carnet != Carnet)
-//	{
-//	  anterior = Temp;
-//	  Temp = Temp->proximo;
-//	}
-//    }
-//  if (Temp != NULL)
-//    {				/* Encontro empleado con esa cedula */
-//      anterior->proximo = Temp->proximo;
-//      free (Temp->Nombre);	/* libera espacio del Nombre primero! */
-//      free (Temp);		/* libera espacio de Empleado eliminado */
-//      return l;
-//    }
-//  else
-//    {				/* No se encontro empleado con esa cedula */
-//      printf ("\n\nEste estudiante no se encuentra en la nomina\n\n");
-//      return l;
-//    }
-//}
-//
-///* 
-// * ===  FUNCTION  ======================================================================
-// *       Nombre:  Consultar
-// *  Descripcion:  Lista todos los estudiantes con nora mayor a un corte de notas
-// *  		Entrada:  Lista l a imprimir
-// *  							Entero corte para el corte de nota
-// * =====================================================================================
-// */
-///* Consultar
-// * Imprime en pantalla los datos de todos los empleados existentes
-// * l: lista a imprimir
-// */
-//void
-//Consultar (Lista l, int corte)
-//{
-//  if (l == NULL)
-//    {
-//      printf ("\n\nNo hay estudiantes registrados.\n\n");
-//    }
-//  else
-//    {
-//      printf ("\n\nLista de estudiantes registrados: \n\n");
-//      while (l != NULL && l->Nota > corte)
-//	{
-//	  printf ("%s%s\n", "Nombre: ", l->Nombre);
-//	  printf ("%s%d\n", "Nota: ", l->Nota);
-//	  printf ("%s%d\n", "Carnet: ", l->Carnet);
-//	  if (l->proximo != NULL)
-//	    {
-//	  		printf ("--------------------------------------\n");
-//	      l = l->proximo;
-//	    }
-//	  else
-//	    {
-//	      printf ("\n\n\n");
-//	      return;
-//	    }
-//	}
-//    }
-//}
-///* ===  FUNCTION  ======================================================================
-// *       Nombre:  EliminarEst
-// *  Descripción:  Elimina un estudiante de la lista identificado por su carnet.
-// *  							Si el estudiante no se encuentra en la lista ésta no se modifica y se 
-// *  							indica al usuario.
-// *			Entrada:	Lista l en la cual se eliminará el estudiante.
-// *								Entero Carnet del estudiante a eliminar
-// *			 Salida:  Lista con la eliminación realizada
-// * =====================================================================================
-// */
-//int
-//Verificar (Lista l, int Carnet)
-//{
-//  Lista Temp;
-//  if (l == NULL)
-//    return 0;	
-//  if (Carnet == l->Carnet)
-//    {
-//      return 1;
-//    }
-//  else
-//    {	
-//      Temp = l->proximo;
-//      while (Temp != NULL && Temp->Carnet != Carnet)
-//				{
-//					Temp = Temp->proximo;
-//				}
-//			if (Temp->Carnet == Carnet){
-//				return 1;
-//			}
-//    }
-////  if (Temp != NULL)
-////    {				/* Encontro empleado con esa cedula */
-////      anterior->proximo = Temp->proximo;
-////      free (Temp->Nombre);	/* libera espacio del Nombre primero! */
-////      free (Temp);		/* libera espacio de Empleado eliminado */
-////      return l;
-////    }
-////  else
-////    {				/* No se encontro empleado con esa cedula */
-////      printf ("\n\nEste estudiante no se encuentra en la nomina\n\n");
-////      return l;
-////    }
-//}
